53afab5936cda4365b67e6f88eee2f08ac929d8f
AlvyneZ <alvynew@gmail.com>
2023-06-01 16:51:19 +0300

    Better implementation of perfect checker

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,23 +1,31 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_balance_height - Measures the height of a binary tree
- *  including the current node in the measurement
+ * binary_tree_perfect_height - Measures the height of a binary tree
+ *  including the current node in the measurement only if its sibling
+ *  subtrees are perfect, then updates the perfect state of the tree
  *
  * @tree: pointer to the root node of the tree
+ * @perfect: [out] whether the subtrees are perfect
  *
  * Return: the height of the tree
  */
-static size_t binary_tree_balance_height(const binary_tree_t *tree)
+static size_t binary_tree_perfect_height(const binary_tree_t *tree,
+		int *perfect)
 {
-	if (tree == NULL)
+	size_t hl, hr = 0;
+
+	if ((tree == NULL) || (!(*perfect)))
 		return (0);
-	return (1 +
-		__MAX(
-			binary_tree_balance_height(tree->left),
-			binary_tree_balance_height(tree->right)
-		)
-	);
+	hl = binary_tree_perfect_height(tree->left, perfect);
+	if (*perfect)
+	{
+		hr = binary_tree_perfect_height(tree->right, perfect);
+		if (hl == hr)
+			return (hl + 1);
+		*perfect = 0;
+	}
+	return (0);
 }
 
 /**
@@ -30,21 +38,10 @@ static size_t binary_tree_balance_height(const binary_tree_t *tree)
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
+	int perfect = 1;
+
 	if (tree == NULL)
 		return (0);
-	if (binary_tree_balance_height(tree->left) !=
-			binary_tree_balance_height(tree->right))
-		return (0);
-	if (tree->left)
-	{
-		if (tree->right)
-			return (
-				binary_tree_is_perfect(tree->left) &&
-				binary_tree_is_perfect(tree->right)
-			);
-		return (0);
-	}
-	if (tree->right)
-		return (0);
-	return (1);
+	binary_tree_perfect_height(tree, &perfect);
+	return (perfect);
 }
