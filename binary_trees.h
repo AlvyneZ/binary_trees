e03386556b6491e07bcecc9801616810dd7cf70e
AlvyneZ <alvynew@gmail.com>
2023-06-01 14:55:12 +0300

    Checking if root or leaf node

--- binary_trees.h
+++ binary_trees.h
@@ -28,10 +28,17 @@ typedef struct binary_tree_s avl_t;
 
 typedef struct binary_tree_s heap_t;
 
+/* Functions for display of the binary tree */
 void binary_tree_print(const binary_tree_t *);
+
+/* Functions for creation and deletion of nodes of the tree */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
 void binary_tree_delete(binary_tree_t *tree);
+
+/* Fucntions for checking type of nodes of the tree */
+int binary_tree_is_leaf(const binary_tree_t *node);
+int binary_tree_is_root(const binary_tree_t *node);
 
 #endif /* BINARY_TREES_H_INC_ */
