276b9d512023d0b8a9df437d881b94ea1e229bdd
AlvyneZ <alvynew@gmail.com>
2023-06-01 15:41:17 +0300

    Counting different types of nodes

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_nodes - Counts the number of nodes with children
+ *  in a binary tree
+ *
+ * @tree: pointer to the root node of the tree
+ *
+ * Return: the number of nodes with children
+ */
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+	if (tree->left || tree->right)
+		return (1 +
+			binary_tree_nodes(tree->left) +
+			binary_tree_nodes(tree->right)
+		);
+	return (0);
+}
